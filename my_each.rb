b9d0985844b6b86da0bd49218f619691f3d7563f
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 23:52:20 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+  def
+  collection = [2,4,6,8]
+  my_each(collection) do |i|
+    puts |i|
+end
+ def 
+   while i < array.length 
+      yield array [i]
+      i = i + 1 
+      
+  end 
 end
